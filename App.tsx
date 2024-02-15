3e0f9db67175fe94b7507dcd9abb62b39fae9c8d
Projectyuuri07 <projectyuuri07@gmail.com>
2024-02-15 16:12:17 -0300

    Update

--- App.tsx
+++ App.tsx
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text, Button, StyleSheet } from "react-native"
+import { StatusBar } from "expo-status-bar"
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+     <StatusBar style="dark" />
+      <Text>hello, world</Text>
+      <Text>hello, world</Text>
+      <Button title="Press me" onPress={() => alert("Button pressed!")} />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    backgroundColor: "#ffb45",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+})
+
+export default App
